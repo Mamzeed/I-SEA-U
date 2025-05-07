977b1ed53fc1f6e46771ddd97ae21b2d85a1b922
Thaenpong <zxcvbhgy0@gmail.com>
2025-05-07 20:27:27 +0700

    mam update7/5/68

--- app_frontend/pages/SEA3/index.js
+++ app_frontend/pages/SEA3/index.js
@@ -5,7 +5,7 @@ export default function SEA2() {
   const [newsList, setNewsList] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:3342/api/news/category/Maritime Crime/')
+    fetch('http://localhost:3342/api/news/category/MaritimeCrime/')
       .then((res) => res.json())
       .then((data) => setNewsList(data))
       .catch((err) => console.error('Error fetching SEA2 news:', err));
