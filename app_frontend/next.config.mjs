440b6dc73a7f3e2c15cd9b5a6172778db236915d
Thaenpong <zxcvbhgy0@gmail.com>
2025-05-15 17:19:36 +0700

    99

--- app_frontend/next.config.mjs
+++ app_frontend/next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/login',
+        permanent: false, // หรือ true ถ้าต้องการ redirect แบบถาวร
+      },
+    ];
+  },
 };
 
 export default nextConfig;
